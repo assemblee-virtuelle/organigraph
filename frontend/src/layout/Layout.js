37ca3c381d3abeba0d66b8eae96b4aefb6bc7f03
srosset81 <srosset81@gmail.com>
2022-08-02 18:48:24 +0200

    Remove temporarily user login

--- frontend/src/layout/Layout.js
+++ frontend/src/layout/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Notification } from 'react-admin';
 import { Grid, Box, useMediaQuery, ThemeProvider, makeStyles, Typography } from '@material-ui/core';
-import { UserMenu, LogoutButton } from '@semapps/auth-provider';
+// import { UserMenu, LogoutButton } from '@semapps/auth-provider';
 import AppBar from './AppBar';
 import ScrollToTop from './ScrollToTop';
 
@@ -36,7 +36,7 @@ const Layout = ({ appBar, logout, theme, title, children }) => {
             </Grid>
             <Grid item xs={6}>
               <Box pr={1}>
-                <UserMenu logout={<LogoutButton />} classes={{ user: classes.userMenu }} />
+                {/*<UserMenu logout={<LogoutButton />} classes={{ user: classes.userMenu }} />*/}
               </Box>
             </Grid>
           </Grid>
